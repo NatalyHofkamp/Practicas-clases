b769899b3eeace4213662102919d0ea97e4c7b12
NatalyHofkamp <101135875+NatalyHofkamp@users.noreply.github.com>
2022-06-10 18:28:36 -0300

    Add files via upload

--- 31_05.py
+++ 31_05.py
@@ -50,10 +50,137 @@ class Regla(Util):
 
 #Diseñar la clase Billetera, la billetera tiene capacidad 
 #Diseñar la clase Billete, tiene denominacion 1,5,10,50
-#Se puede meter billetes en la billetera
-#Se puede quitar billetes de la billetera
+#Se puede meter billetes en la billetera.
+#Se puede quitar billetes de la billetera.
 #Se pide unir billeteras.
 #construir una billetera con los billetes de 2 billeteras.
 #al fin, las billeteras origen quedan vacias.
-
 #Algoritmo para tener la menor cantidad de billetes.
+
+class Billetera (object):
+    def __init__ (self,capacidad):
+        self.capacidad = capacidad
+        self.cosas_dentro =[]
+    def meter  (self,billete):
+        if not isinstance(billete,Billete):
+            print('La billetera solo acepta billetes')
+        elif len (self.cosas_dentro)>= self.capacidad:
+            print('la billetera esta llena')
+        else:
+            self.cosas_dentro.append(billete)
+    def quitar (self,billete):
+        if billete in self.cosas_dentro:
+            self.cosas_dentro.remove(billete)
+
+    def __repr__(self) -> str:
+        r= 'billete de capacidad'+str(self.capacidad)+'\n'
+
+        r+= 'Contenido'+str(len(self.cosas_dentro))+'\n'
+        for billete in self.cosas_dentro:
+            r+=''+billete.__repr__()
+        return r 
+    def vaciar(self):
+        self.cosas_dentro=[]
+    #Crear una billetera a partir de otras dos.
+    def unir (self,other):
+        if not isinstance (other,Billetera):
+            print('Solo pueden unirse dos billeteras.')
+        nueva = Billetera(self.capacidad+other.capacidad)
+        for billete  in self.cosas_dentro:
+            nueva.meter(billete)
+        self.vaciar()
+
+        for billete in other.cosas_dentro:
+            nueva.meter(billete)
+        other.vaciar()
+        return nueva
+    def cambiar(self):
+        #dada una billetera con ciertos billetes de denominacion baja,reducir la canti de billetes al minimo conservando el valor.
+        billetes ={}
+        valor=[50,10,5,1]
+        while v>0:
+            if v-valor[0] >=0:
+                v-=valor[0]
+                if valor[0] not in billetes:
+                    billetes[valor[0]]=1
+                else:
+                    billetes[valor[0]]+=1
+            else:
+                del valor[0]
+        for key,value  in billetes.items():
+            print(f'hay {value} billetes de {key}')
+
+#%%
+#otra forma correcta de cambio
+    def cambiar(valor):
+        billetes =[]
+        denominaciones =[50,10,5,1]
+        i=0
+        while v>0:
+            if v>= denominaciones[i]
+                billetes.append(denominaciones[i])
+                v-=denominaciones[i]
+            else:
+                i+=1
+        return billetes
+    def valor (self):
+        v=0
+        for billete in self.cosas_dentro:
+            v+=billete.denominacion
+        return v
+
+    def partir_en_dos (self):
+        # crear dos billeteras con los billetes de self.
+        #tal que los valores de ambas sean semejantes.
+
+        while i < len (billetes):
+            if valores[0]+ billetes[i]<= valores[1]:
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        billeteras= [Billetera(self.capacidad),Billetera(self.capacidad)]
+        billetes ={}
+        denominaciones =[50,10,5,1]
+        for billete in self.cosas_dentro:
+            if billete not in billetes:
+                billetes[billete]=1
+            else:
+                billetes[billetes]+=1
+
+        mitad = self.valor//2
+
+        if mitad - denominaciones[0]>=0 and denominaciones[0]>0 :
+            Billetera.meter(denominaciones[0])
+            mitad
+
+        return billeteras
+
+
+
+class Billete (object):
+    #Agregar restriccion para la denominacion
+    def __init__(self,denominacion):
+        if denominacion not in (1,5,10,50):
+            self.denominacion = None
+        else:
+            self.denominacion = denominacion
+    def __repr__(self) -> str:
+        return 'Billete de:' +str(self.denominacion)
+
+
+
+
+
